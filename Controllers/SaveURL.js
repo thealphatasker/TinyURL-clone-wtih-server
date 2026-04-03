b88a6bc06abac8b73d6f7f3581095c3a60a786ba
Ahsan <thefirstblood29@gmail.com>
2026-04-03 15:34:42 +0500

    DB URL update

--- Controllers/SaveURL.js
+++ Controllers/SaveURL.js
@@ -7,7 +7,7 @@ export const SaveURL = async (req, res) => {
     const shortId = generateShortId(7);
     const newURL = new URLs({ longUrl: longUrl, shortId: shortId });
     await newURL.save();
-    const shortURL = `https://tinyurl-server-production-e5d7.up.railway.app/${shortId}`;
+    const shortURL = `https://tinyurl1.up.railway.app/${shortId}`;
     res.status(200).json({
       ok: true,
       shortURL: shortURL,
